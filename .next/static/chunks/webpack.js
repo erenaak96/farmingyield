5abd8e00f1bcdc36f54316a76589c73121e03cfd
Chelsea <erenakkaya@Chelseas-MacBook-Air.local>
2022-04-22 10:18:38 +0300

    modal fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c79418c49c6cfe8c"; }
+/******/ 		__webpack_require__.h = function() { return "88b4cedb06f32a64"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.dcc5eaae0e4c6844.hot-update.js
+++ .next/static/webpack/webpack.dcc5eaae0e4c6844.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8f55885d98daa9af"; }
+/******/ 	__webpack_require__.h = function() { return "226595d1eacc8207"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
